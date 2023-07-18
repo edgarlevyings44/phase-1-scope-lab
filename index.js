c2390c4f71afe87b8fcbeeec38c8dfd5e0766a3b
Levy <edgarlevy81@gmail.com>
2023-07-19 01:48:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+customerName;
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+
+  }
+
+function setBestCustomer (){
+    bestCustomer='not bob';
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Assessment.';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'Assignment';
+}
